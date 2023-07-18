231cb2b9a521d6f9f9600c65f003963112a3429f
Zheng Xiang <wong.zhengxiang@gmail.com>
2023-07-18 16:54:47 +0800

    EDIT: delete allocated forms

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zwong <zwong@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 15:55:14 by zwong             #+#    #+#             */
-/*   Updated: 2023/07/18 16:40:41 by zwong            ###   ########.fr       */
+/*   Updated: 2023/07/18 16:54:05 by zwong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,26 +34,36 @@ int	main(void)
 
 	form = bob.makeForm("presidential", "forgive jacob");
 	sign_and_exec(form);
-	std::cout << std::endl;
-	form = bob.makeForm("robotomy", "create terminator 2");
-	sign_and_exec(form);
-	std::cout << std::endl;
-	form = bob.makeForm("shrubbery", "amazon");
-	sign_and_exec(form);
+	delete form;
 	std::cout << std::endl;
 
+	form = bob.makeForm("robotomy", "create terminator 2");
+	sign_and_exec(form);
+	delete form;
 	std::cout << std::endl;
+
+	form = bob.makeForm("shrubbery", "amazon");
+	sign_and_exec(form);
+	delete form;
+	std::cout << std::endl;
+
 	std::cout << RED << "----- INTERN CREATE BAD FORMS-----" << RESET << std::endl << std::endl;
 	try {
 		form = bob.makeForm("presidential", ""); // can create but without target will be ""
 		sign_and_exec(form);
+		delete form;
 		std::cout << std::endl;
+		
 		form = bob.makeForm("", "noname target"); // cannot create with "" no form name
 		sign_and_exec(form);
+		delete form;
 		std::cout << std::endl;
+		
 		form = bob.makeForm("notarealform", ""); // cannot create with invalid form name
 		sign_and_exec(form);
+		delete form;		
 		std::cout << std::endl;
+		
 	} catch (std::exception &err) {
 		std::cout << "Failed with catch err: " << err.what() << std::endl;
 	}
