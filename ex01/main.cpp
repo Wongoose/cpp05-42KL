b9b44848c21e96ce46951c37cfeb59ce79a0d289
Zheng Xiang <wong.zhengxiang@gmail.com>
2023-09-25 14:55:01 +0800

    REVIEW: Added eval comments (ex01)

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: zwong <zwong@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 13:16:00 by zwong             #+#    #+#             */
-/*   Updated: 2023/07/13 14:16:56 by zwong            ###   ########.fr       */
+/*   Updated: 2023/09/25 14:54:34 by zwong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,21 +36,23 @@ int main(void) {
 	
 	Form state_form("State form", 75, 150);
 	Form presidential_form("Presidential form", 74, 150);
+	// show copy
 	Form kuala_lumpur(state_form);
 	Form united_states(presidential_form);
 
-	std::cout << std::endl << GREEN << "----- FROM STATUSES -----" << std::endl << std::endl;
-	std::cout << state_form << std::endl
-						<< presidential_form << std::endl
-						<< kuala_lumpur << std::endl
-						<< united_states << std::endl;
+	std::cout << std::endl << YELLOW << "----- FORM STATUSES w COPIED -----" << std::endl << std::endl;
+	std::cout << state_form << std::endl;
+	std::cout << presidential_form << std::endl;
+	std::cout << kuala_lumpur << std::endl;
+	std::cout << united_states << std::endl;
 
 	std::cout << std::endl;
 	
 	abby.signForm(state_form);
 	abby.signForm(presidential_form);
 
-	std::cout << std::endl << GREEN << "----- FROM STATUSES -----" << std::endl << std::endl;
+	std::cout << std::endl << MAGENTA << "----- FORM STATUSES w COPIED -----" << std::endl << std::endl;
+	// shows that the forms are deep copies
 	std::cout << state_form << std::endl;
 	std::cout << presidential_form << std::endl;
 	std::cout << kuala_lumpur << std::endl;
